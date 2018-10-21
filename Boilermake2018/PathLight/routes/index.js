52253138c12ff69fc87a613bf30d9b949c693e7e
hyip8000 <hyip@purdue.edu>
2018-10-20 22:31:14 -0400

    Merge pull request #2 from gavin-ng/beverly
    Got map and text fields (starting location fixed)

--- Boilermake2018/PathLight/routes/index.js
+++ Boilermake2018/PathLight/routes/index.js
@@ -38,4 +38,13 @@ router.post('/login', (req, res, next) => {
   });
 });
 
+router.get('/logout', (req, res, next) => {
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) return next(err);
+    });
+  }
+  return res.redirect('/');
+});
+
 module.exports = router;
---
+++ Boilermake2018/PathLight/routes/mapdata.js
@@ -0,0 +1,11 @@
+// HERE IS WHERE THE PYTHON FILE GOES.
+// YOU ALSO NEED TO RENAME THE .JS FILE BECAUSE YOURS IS .PY
+
+
+//THINGS TO DO
+
+// 1. Create new hbs file to show map (remove map from index.hbs)
+// - basically copy over the code that displays map in index.hbs to newhbs file (under Views)
+// 2. Figure a way to pass user input from <form> in index.hbs to this FILE
+// 3. Figure a way to pass your answer to new hbs FILE
+// 4. Pretty much it really - make it pretty Aspen
